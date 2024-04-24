83af2aa47175e3e7cd3c14815765d94519f58789
dimty <dimty.ztoa@gmail.com>
2024-04-24 16:07:13 +0700

    set prisma and next-auth

---
+++ auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from "./lib/prisma"
+import Google from 'next-auth/providers/google';
+import GitHub from 'next-auth/providers/github';
+ 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [Google, GitHub],
+})
